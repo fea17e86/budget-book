ab40e2fc5d008b4c3171d5a7b112154ff2075d03
fea17e86 <fea17e86@mail.de>
2015-10-22 16:13:42 +0200

    removed semantic-ui, have to finish adding bootstrap, font-awesome fonts are not copied anymore; started gulp-express for the server

---
+++ src/server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  database: {
+    accounts: 'database/accounts.db',
+    proxies: 'database/proxies.db',
+    transactions: 'database/transactions.db',
+    users: 'database/users.db'
+  }
+};
---
+++ src/server/models/transactions.js
@@ -0,0 +1,31 @@
+/*
+ * Transaction
+ * ===========
+ *  date        : Date
+ *  amount      : Number
+ *  type        : String (Transactions.type)
+ *  account     : String (Account._id)
+ *  counterpart {
+ *    _id       : String (Account._id || Proxy._id)
+ *    type      : 'Accounts.modelType' || Proxy.modelType
+ *  }
+ *  status      : String (Transactions.status)
+ *  notes       : String
+ *  tags        : String[]
+ *  _type       : 'transaction' (Model)
+ *  _id         : String (nedb)
+ */
+
+var config = require('../config'),
+    Model = require('../model.js');
+
+var Transactions = new Model('transaction', config.database.transactions);
+
+Transactions.type = {
+  deposit: 'deposit',
+  transfer: 'transfer',
+  withdrawal: 'withdrawal'
+};
+Transactions.type.list = [Transactions.type.deposit, Transactions.type.transfer, Transactions.type.withdrawal];
+
+module.exports = Transactions;
