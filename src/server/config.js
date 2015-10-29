bbf5298b57cecd949bac870035271ba4436c8a3a
fea17e86 <fea17e86@mail.de>
2015-10-29 13:21:47 +0100

    changed server/config.js to store database in dist folder

--- src/server/config.js
+++ src/server/config.js
@@ -2,10 +2,10 @@ module.exports = {
   server: {
     port: 3000,
     database: {
-      accounts: 'database/accounts.db',
-      proxies: 'database/proxies.db',
-      transactions: 'database/transactions.db',
-      users: 'database/users.db'
+      accounts: './database/accounts.db',
+      proxies: './database/proxies.db',
+      transactions: './database/transactions.db',
+      users: './database/users.db'
     }
   }
 };
